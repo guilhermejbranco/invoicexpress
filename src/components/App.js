eab5e47f2d2ad519488853c53a1c960b270a1c32
Guilherme Branco <guilhermejbranco@Guilhermes-MBP.lan>
2020-01-13 19:48:16 +0000

    Created list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from '../assets/resources/logo.svg';
 import '../styles/App.scss';
+import List from './List'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="btn btn-primary">Learn React</div>
-        </a>
+        <List></List>
       </header>
     </div>
   );
---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/List.scss';
+
+function List() {
+  return (
+    <div className="List">
+      List of documents
+    </div>
+  );
+}
+
+export default List;
