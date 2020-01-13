e6b84b543c72e77e1dc5b32cffa09e3815a2b8ff
Guilherme Branco <guilhermejbranco@Guilhermes-MBP.lan>
2020-01-13 19:31:09 +0000

    Added Bootstrap support

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <div className="btn btn-primary">Learn React</div>
         </a>
       </header>
     </div>
