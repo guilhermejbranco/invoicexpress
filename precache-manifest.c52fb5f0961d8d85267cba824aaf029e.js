8000505be25726971d3fd636bbb1866c4df01174
Guilherme Branco <guilhermejbranco@Guilhermes-MBP.lan>
2020-01-24 02:01:04 +0000

    Updates

---
+++ precache-manifest.c52fb5f0961d8d85267cba824aaf029e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fce0a49123a76f6d3786398acdf7ad5",
+    "url": "/invoicexpress/index.html"
+  },
+  {
+    "revision": "9b52c1a363c98283e780",
+    "url": "/invoicexpress/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c8dd66b25861f43feada",
+    "url": "/invoicexpress/static/css/main.78de7119.chunk.css"
+  },
+  {
+    "revision": "9b52c1a363c98283e780",
+    "url": "/invoicexpress/static/js/2.7a424000.chunk.js"
+  },
+  {
+    "revision": "16a4527db387cf31fd19131d298aca07",
+    "url": "/invoicexpress/static/js/2.7a424000.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8dd66b25861f43feada",
+    "url": "/invoicexpress/static/js/main.5c9e1ee3.chunk.js"
+  },
+  {
+    "revision": "3f77e531252f6f0fd320",
+    "url": "/invoicexpress/static/js/runtime-main.72dddc95.js"
+  }
+]);
